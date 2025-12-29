6e41cbb2cc43a10b0e650774838479d69b94cf0d
Jacob Starko <105564503+Fatalerorr69@users.noreply.github.com>
2025-12-29 09:00:01 +0100

    Add basic.sh for system update and package installation

---
+++ basic.sh
@@ -0,0 +1,8 @@
+sudo apt update && sudo apt full-upgrade -y
+sudo apt install -y \
+  curl wget git htop tmux \
+  ca-certificates gnupg lsb-release \
+  software-properties-common \
+  unzip zip rsync \
+  ufw fail2ban \
+  chrony
